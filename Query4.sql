0cd9807b4b04bb5e9250dedd5a88b37fccf0e150
polyakovael <polyakovaaaa10@gmail.com>
2024-06-07 18:14:52 +0700

    Add files via upload
    Добавили более сложные примеры соединения таблиц

--- Query4.sql
+++ Query4.sql
@@ -51,3 +51,56 @@ FROM
     `groups` ON (students.idgroups = `groups`.idgroups)
 WHERE
     napr LIKE 'И%';
+    
+/* Использование составных условий при соединении таблиц*/
+SELECT 
+    `students`.`name`, `groups`.`napr`, `groups`.`prof`
+FROM
+    students,
+    `groups`
+WHERE
+    shifr_napr IN ('09.03.02' , '44.03.02')
+        AND students.idgroups = `groups`.idgroups;
+        
+/* Испозьзование операторов сравнения при соединении*/
+SELECT 
+    `students`.`name`, `groups`.`napr`, `groups`.`prof`
+FROM
+    students,
+    `groups`
+WHERE
+    shifr_napr > '09.03.02'
+        AND students.idgroups < `groups`.idgroups;
+        
+/* Соединение таблицы с собой */
+SELECT 
+    `first`.`name`, `second`.`name`
+FROM
+    students `first`,
+    students `second`
+WHERE
+    `first`.idgroups = `second`.idgroups
+        AND `first`.`name` > `second`.`name`;
+        
+/* Поиск образцов */
+SELECT 
+    a.`name` as 'Группа 3.054.2.19', b.`name`as 'Группа 3.114.2.21', c.`name` as 'Группа 3.008.1.20'
+FROM
+    students a,
+    students b,
+    students c
+WHERE
+    a.idgroups = 2 AND b.idgroups = 3
+        AND c.idgroups = 5;
+
+/* Соединение таблиц, не входящих в результат запроса */
+SELECT 
+    `students`.`name`, `students`.`dob`
+FROM
+    students,
+    `groups`
+WHERE
+    shifr_napr = '09.03.02'
+        AND students.idgroups = `groups`.idgroups;
+        
+/* */
